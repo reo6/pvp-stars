db373a7c38779674d71ed02f6a3242c967012704
ramazanemreosmanoglu <ramazanemre@protonmail.com>
2021-02-20 16:49:43 +0000

    Fundamental Structure

---
+++ src/components/Start.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class Start extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <main className="AppHeader">
+                    <h1 className="stick-font display-3">Welcome to PvP Game!</h1>
+                    <p>Just type your nickname to start playing!</p>
+                </main>
+            </div>
+        );
+    }
+}
