abf9a8ef3902a2c8ad0c7cffd15ff937f3da6565
ramazanemreosmanoglu <ramazanemre@protonmail.com>
2021-02-22 12:50:43 +0000

    Skill Point Distribution Part

--- src/components/Game/SkillPoints.jsx
+++ src/components/Game/SkillPoints.jsx
@@ -1,6 +1,28 @@
 import React from "react";
+import {
+    Col,
+    Row,
+} from "react-bootstrap";
+
+function SkillPoint(props) {
+    const increase = function() {}
+    const decrease = function() {}
+    
+    return (
+        <Col>
+            <Row>
+                <button type="button" className='minus'>-</button>
+                {props.sp_value}
+                <button type="button" className='plus'>+</button>
+            </Row>
+        </Col>
+    )
+}
 
 export default class SkillPoints extends React.Component {
+    state = {
+        
+    }
     render() {
         return (
             <>
